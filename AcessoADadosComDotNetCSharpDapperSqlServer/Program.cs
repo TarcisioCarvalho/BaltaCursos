253ebc21e36b93372349aae50b0b245bc9bf1f53
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-05-09 20:13:14 -0300

    Procedure Dapper

--- AcessoADadosComDotNetCSharpDapperSqlServer/Program.cs
+++ AcessoADadosComDotNetCSharpDapperSqlServer/Program.cs
@@ -1,4 +1,5 @@
-﻿using Dapper;
+﻿using System.Data;
+using Dapper;
 using Microsoft.Data.SqlClient;
 
 // See https://aka.ms/new-console-template for more information
@@ -6,8 +7,12 @@ using Microsoft.Data.SqlClient;
 const string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=balta;Trusted_Connection=True;MultipleActiveResultSets=True;";
 using(var connection = new SqlConnection(connectionString))
 {
-   UpdateCategory(connection);
-   ListCategories(connection);
+   //UpdateCategory(connection);
+  // DeleteCategory(connection);
+  // CreateManyCategory(connection);
+   //ListCategories(connection);
+
+   ExecuteProcedure(connection);
 }
 
 
@@ -63,4 +68,82 @@ static void UpdateCategory(SqlConnection connection)
   });
 
   System.Console.WriteLine($"Registros atualizados {rows}");
+}
+
+
+static void DeleteCategory(SqlConnection connection)
+        {
+            var deleteQuery = "DELETE [Category] WHERE [Id]=@id";
+            var rows = connection.Execute(deleteQuery, new
+            {
+                id = new Guid("0a84a194-89be-453e-aa06-59582a3018e4"),
+            });
+
+            Console.WriteLine($"{rows} registros excluídos");
+        }
+
+
+ static void CreateManyCategory(SqlConnection connection)        {
+
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var category2 = new Category();
+            category2.Id = Guid.NewGuid();
+            category2.Title = "Categoria Nova";
+            category2.Url = "categoria-nova";
+            category2.Description = "Categoria nova";
+            category2.Order = 9;
+            category2.Summary = "Categoria";
+            category2.Featured = true;
+
+            var insertSql = @"INSERT INTO 
+                    [Category] 
+                VALUES(
+                    @Id, 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured)";
+
+            var rows = connection.Execute(insertSql, new[]{
+                new
+                {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                },
+                new
+                {
+                    category2.Id,
+                    category2.Title,
+                    category2.Url,
+                    category2.Summary,
+                    category2.Order,
+                    category2.Description,
+                    category2.Featured
+                }
+            });
+            Console.WriteLine($"{rows} linhas inseridas");
+        }
+
+static void ExecuteProcedure(SqlConnection connection)
+{
+  var sql = "[spDeleteStudent]";
+  var pars = new { StudentId = "c55390d4-71dd-4f3c-b978-d1582f51a327" };
+  var affectedRows = connection.Execute(sql,pars,commandType:CommandType.StoredProcedure);
+
+  System.Console.WriteLine($"Linhas afetadas {affectedRows}");
 }
