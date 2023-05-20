dee36b31a6683a3250b6cb9a57e8660dfbf5a423
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2023-05-20 19:26:42 -0300

    Crud role e user

--- Blog/Repositories/RoleRepository.cs
+++ Blog/Repositories/RoleRepository.cs
@@ -14,5 +14,7 @@ public class RoleRepository
     public IEnumerable<Role> Get() => _connection.GetAll<Role>();
     public Role Get(int id) => _connection.Get<Role>(id);
     public void Create(Role role) => _connection.Insert<Role>(role);
+    
+    public void Update(Role role) => _connection.Update<Role>(role);
     public void Delete(Role role) => _connection.Delete<Role>(role);
 }
--- Blog/Repositories/UserRepository.cs
+++ Blog/Repositories/UserRepository.cs
@@ -14,6 +14,7 @@ public class UserRepository
     public IEnumerable<User> Get() => _connection.GetAll<User>();
     public User Get(int id) => _connection.Get<User>(id);
     public void Create(User user) => _connection.Insert<User>(user);
+    public void Update(User user) => _connection.Update<User>(user);
     public void Delete(User user) => _connection.Delete<User>(user);
     
 }
